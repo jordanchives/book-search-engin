0a2095c8bf6079662e5d075d1727ffd931829871
Jordan M <jormmiller@gmail.com>
2024-07-14 17:25:55 -0400

    finished auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,47 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { GraphQLError } = require("graphql");
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+// Set token secret and expiration date
+const secret = "superdupersecrettimesbro";
+const expiration = "2h";
 
 module.exports = {
-  // function for our authenticated routes
+  // Custom authentication error for GraphQL
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
+    extensions: {
+      code: "UNAUTHENTICATED",
+    },
+  }),
+
+  // Middleware for authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+    // Allow token to be sent via req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return next();
     }
 
-    // verify token and get user data out of it
+    // Verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    } catch (err) {
+      console.log("Invalid token");
+      return res.status(401).json({ message: "Invalid token" });
     }
 
-    // send to next endpoint
     next();
   },
+
+  // Function to sign a token
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
